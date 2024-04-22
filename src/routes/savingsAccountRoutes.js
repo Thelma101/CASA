723433303236c92d8270a82ad80a2dfb5a95140e
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 14:32:39 +0100

    v1-deposit-id-fix

--- src/routes/savingsAccountRoutes.js
+++ src/routes/savingsAccountRoutes.js
@@ -3,5 +3,9 @@ const app = express.Router();
 const savingsAccountController = require('../controllers/savingsAccountController');
 
 app.post('/create', savingsAccountController.createSavingsAccount);
+app.get('/:id', savingsAccountController.getSavingsAccountById);
+app.post('/deposit/:id', savingsAccountController.depositIntoSavings);
+app.post('/withdraw/:id', savingsAccountController.withdrawFromSavings);
+app.delete('/delete/:id', savingsAccountController.deleteSavingsAccount);
 
 module.export = app;
