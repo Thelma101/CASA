073c45c8e2a5442b458cf91811560043b4e00a8c
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 10:38:56 +0100

    v1

---
+++ src/controllers/userController.js
@@ -0,0 +1,79 @@
+// const cifService = require('../services/cifService');
+
+// // Controller function to create a new CIF
+// exports.createCustomer = async (req, res) => {
+//     try {
+//         const { name, email, address } = req.body;
+//         const customer = await cifService.createCustomer(name, email, address);
+//         res.status(201).json(customer);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+// // Controller function to get CIF details by ID
+// exports.getCustomer = async (req, res) => {
+//     try {
+//         const customerId = req.params.customerId;
+//         const customer = await cifService.getCustomerById(customerId);
+//         if (!customer) {
+//             return res.status(404).json({ message: 'Customer not found' });
+//         }
+//         res.json(customer);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+
+// controllers/cifController.js
+
+const cifService = require('../services/cifService');
+
+// Controller function to create a new CIF
+exports.createCustomer = async (req, res) => {
+    try {
+        const {
+            date,
+            biometricId,
+            title,
+            firstName,
+            middleName,
+            lastName,
+            DOB,
+            email,
+            phoneNumber,
+            gender,
+            maritalStatus,
+            countryOfResidence,
+            address,
+            occupation,
+        } = req.body;
+
+        // Concatenate full name if middleName is present
+        const fullName = middleName
+            ? `${firstName} ${middleName} ${lastName}`
+            : `${firstName} ${lastName}`;
+
+        const customer = await cifService.createCustomer({
+            date,
+            biometricId,
+            title,
+            firstName,
+            fullName,
+            lastName,
+            DOB,
+            email,
+            phoneNumber,
+            gender,
+            maritalStatus,
+            countryOfResidence,
+            address,
+            occupation,
+        });
+
+        res.status(201).json(customer);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
