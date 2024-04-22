6fbb7bd929ee18874c0302016f90b842c816ef57
DCTP-BE <akpata.thelma@gmail.com>
2024-04-23 00:48:54 +0100

    v1-delete-extras

--- src/routes/currentAccountRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const app =  express.Router();
-const currentAccountController = require('../controllers/currentAccountController');
-const currentAccountService = require('../services/currentAccountService');
-
-
-app.post('/create', currentAccountController.createCurrentAccount);
