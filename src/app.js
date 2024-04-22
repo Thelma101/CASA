96a24574092e6fe962c66d00f30af943feb49718
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 13:40:10 +0100

    v1 - casa-endpoint--fix

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const casaRoutes = require('./routes/casaRoutes');
 
 app.use(express.json());
 
 app.use('/cif', userRoutes);
+app.use('/createCasaAccount', casaRoutes);
 
 // Starting the server
 app.listen(3000, () => {
