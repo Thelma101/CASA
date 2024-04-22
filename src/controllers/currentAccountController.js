c094f37cee215dc69ddbe126b6b9c1e5ea382c65
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 16:10:32 +0100

    v1

--- src/controllers/currentAccountController.js
+++ src/controllers/currentAccountController.js
@@ -8,6 +8,13 @@ exports.createCurrentAccount = async (req, res) => {
         const { cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender } = req.body;
         const currentAccount = await currentAccountService.createCurrentAccount({ cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender });
 
+        const customer = await currentAccountService.createCustomer({ cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender });
+
+        if (!customer) {
+            return res.status(400).json({
+                message: 'Customer not created'
+            });
+        }
     }
     catch (error) {
         console.log(error);
