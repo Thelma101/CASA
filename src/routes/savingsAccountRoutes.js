6c8f25d3eef0d0861f9fae1969263157b5c0065a
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 14:30:06 +0100

    v1

--- src/routes/savingsAccountRoutes.js
+++ src/routes/savingsAccountRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express.Router();
 const savingsAccountController = require('../controllers/savingsAccountController');
 
-const app.post('/create', savingsAccountController.createSavingsAccount);
+app.post('/create', savingsAccountController.createSavingsAccount);
 
-module export = app;
+module.export = app;
