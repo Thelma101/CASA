cd16f0303ce72487f3e771d6451acb821e2b96a3
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 15:52:44 +0100

    v1

--- src/controllers/currentAccountController.js
+++ src/controllers/currentAccountController.js
@@ -2,12 +2,9 @@ const currentAccountService = require('../services/currentAccountService');
 const currentAccountRoutes = require('../routes/currentAccountRoutes');
 
 
-export.createCurrentAccount = async (req, res) => {
+exports.createCurrentAccount = async (req, res) => {
 
-    const { cifId,bvn,firstName, middleName, lastName, DOB, email, phoneNumber, gender } = req.body;
-    const currentAccount = await currentAccountService.createCurrentAccount({ cifId,bvn,firstName, middleName, lastName, DOB, email, phoneNumber, gender });
+    const { cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender } = req.body;
+    const currentAccount = await currentAccountService.createCurrentAccount({ cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender });
 
-    const { body } = req;
-    const currentAccount = await currentAccountService.createCurrentAccount(body);
-    res.status(201).json(currentAccount);
 }
