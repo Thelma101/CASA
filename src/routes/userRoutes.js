88f5df6d92e8cd50297f733fbfb31e40a90e47d5
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 11:02:48 +0100

    v2

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,4 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router
+router.post('/create', userController.createCustomer);
+
+router.get('/', userController.getAllCustomers);
+
+router.get('/:id', userController.getCustomerById);
+
+router.put('/:id', userController.updateCustomer);
+
+router.delete('/:id', userController.deleteCustomer);
