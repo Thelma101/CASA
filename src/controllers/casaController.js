5a70a8bc105b0f86edb6d329d12a5736e6402d95
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 14:09:58 +0100

    v1

--- src/controllers/casaController.js
+++ src/controllers/casaController.js
@@ -1,1 +1,16 @@
 const casaService = require('../services/casaService');
+const userService = require('../services/userService');
+
+
+
+export createCasaAccount = async (req, res) => {
+    try {
+        const { userId, ...data } = req.body;
+        const user = await userService.getUserById(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Error creating casa account' });
+    }
+}
