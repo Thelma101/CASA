276d7582df639c6a8e6804f9e7bbc6a20ccde919
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 12:14:09 +0100

    v1 - port-setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
-const usersRoutes = require('./routes/users');
+const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json());
+
+app.use('/cif', userRoutes);
+
+// Starting the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
