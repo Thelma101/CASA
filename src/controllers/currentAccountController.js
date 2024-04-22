a20556ef8c12fca6429ebea235c4b271708daecc
DCTP-BE <akpata.thelma@gmail.com>
2024-04-22 15:53:50 +0100

    v1

--- src/controllers/currentAccountController.js
+++ src/controllers/currentAccountController.js
@@ -4,7 +4,16 @@ const currentAccountRoutes = require('../routes/currentAccountRoutes');
 
 exports.createCurrentAccount = async (req, res) => {
 
-    const { cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender } = req.body;
-    const currentAccount = await currentAccountService.createCurrentAccount({ cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender });
+    try {
+        const { cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender } = req.body;
+        const currentAccount = await currentAccountService.createCurrentAccount({ cifId, bvn, firstName, middleName, lastName, DOB, email, phoneNumber, gender });
+
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
 
 }
