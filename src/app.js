ed6a4227f3933c7a5a2a4f488cdc9bb2a014c951
DCTP-BE <akpata.thelma@gmail.com>
2024-04-23 00:42:47 +0100

    v2-routes-fix

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-const express = require('express');
-// const app = express();
-const app = express.Router();
 
-const userRoutes = require('./routes/userRoutes');
+const express = require('express');
+const app = express(); // Create the Express app
+
+const userRoutes = require('./routes/userController');
 const currentAccountRoutes = require('./routes/currentAccountRoutes');
 const savingsAccountsRoutes = require('./routes/savingsAccountsRoutes');
 
+app.use(express.json()); // Apply middleware
 
-app.use(express.json());
-
-app.use('/cif', userRoutes);
+app.use('/cif', userRoutes); 
 app.use('/createCAA', currentAccountRoutes);
 app.use('/createSBA', savingsAccountsRoutes);
 
-// Starting the server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express.Router();
 const userController = require('../controllers/userController');
 
-app.post('/create', userController.createCustomer);
 
-app.get('/', userController.getAllCustomers);
-
+app.post('/createCAA', userController.createCustomer);
+app.post('/createSBA', userController.createCustomer);
+app.get('/:id', userController.getAllCustomers);
 app.get('/:id', userController.getCustomerById);
-
 app.put('/:id', userController.updateCustomer);
-
 app.delete('/:id', userController.deleteCustomer);
+
+module.exports = app;
